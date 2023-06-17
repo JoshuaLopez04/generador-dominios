06ddda52be094a9cf8c68ce310030c8f3bd119a3
JoshuaLopez04 <105984452+JoshuaLopez04@users.noreply.github.com>
2023-06-17 14:29:33 +0000

    arreglo

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
 var pronombre = ['El','Nosotros','Tu'];
 var adjetivo = ['Excelente', 'pequeño','Grande'];
 var sustantivo = ['locos','mapache','Perro'];
 var add = ".com";
 
-function generardominio (){
-    return pronombre[getRandomInt(3)] + adjetivo[getRandomInt(3)] + sustantivo[getRandomInt(3)] + add;
-}
+for (var i = 0; i < pronombre.length; i++) {
 
-for (var i = 0; i <= 50; i++) {
-    console.log(generardominio());
+    for (var j = 0; j < adjetivo.length; j++){
+
+        for (var l = 0; l < sustantivo.length; l++){
+            console.log(pronombre[i] + adjetivo[j] + sustantivo[l] + add);
+        }
+    }
   }
